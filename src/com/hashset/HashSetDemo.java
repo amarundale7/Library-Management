266abf306d560a32820c0c5bf353c8c79860022f
amardeep <amarundale7@gmail.com>
2023-06-15 23:00:29 +0530

    Hashset Demo Added

---
+++ src/com/hashset/HashSetDemo.java
@@ -0,0 +1,18 @@
+package com.hashset;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet<Integer> set = new HashSet<>();
+		set.add(100);
+		set.add(200);
+		set.add(300);
+		
+		System.out.println(set);
+
+	}
+
+}
